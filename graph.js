e1749db3ae1fbb90e0df2cee0dca9e744247e7cd
dominicbutler <dominic.butler@exonar.com>
2021-02-25 10:59:38 +0000

    scales and axis

--- graph.js
+++ graph.js
@@ -1,6 +1,6 @@
 const getDataset = () => {
     fetch(
-        'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json'
+        'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json'
     )
         .then(response => response.json())
         .then(data => drawGraph(data));
@@ -8,16 +8,16 @@ const getDataset = () => {
 
 getDataset();
 
-const drawGraph = dataset => {
-    console.log(dataset);
+const drawGraph = data => {
+    console.log(data);
     const w = 700;
     const h = 500;
     const p = 50;
 
-    const yearObject = d => new Date(d.Year, 0, 1);
-    const timeObject = d => new Date(1970, 0, 1, 0, 0, d.Seconds);
+    const yearObject = d => new Date(d.year, 0, 1);
+    const monthObject = d => new Date(1970, d.month - 1, 1);
 
-    const color = d3.scaleOrdinal(d3.schemeCategory10);
+    // const color = d3.scaleOrdinal(d3.schemeCategory10);
 
     const svg = d3
         .select('#chart')
@@ -28,16 +28,16 @@ const drawGraph = dataset => {
 
     const xScale = d3
         .scaleTime()
-        .domain(d3.extent(dataset, d => yearObject(d)))
+        .domain(d3.extent(data.monthlyVariance, d => yearObject(d)))
         .range([p, w - p]);
 
     const yScale = d3
         .scaleTime()
-        .domain(d3.extent(dataset, d => timeObject(d)))
+        .domain(d3.extent(data.monthlyVariance, d => monthObject(d)))
         .range([p, h - p]);
 
-    const yAxis = d3.axisLeft(yScale).tickFormat(d3.timeFormat('%M:%S'));
-    const xAxis = d3.axisBottom(xScale);
+    const yAxis = d3.axisLeft(yScale).tickFormat(d3.timeFormat('%b'));
+    const xAxis = d3.axisBottom(xScale).tickFormat(d3.timeFormat('%Y'));
 
     svg.append('g')
         .attr('transform', `translate(${p}, 0)`)
@@ -49,40 +49,40 @@ const drawGraph = dataset => {
         .attr('id', 'x-axis')
         .call(xAxis);
 
-    svg.selectAll('circle')
-        .data(dataset)
-        .enter()
-        .append('circle')
-        .attr('cx', d => xScale(yearObject(d)))
-        .attr('cy', d => yScale(timeObject(d)))
-        .attr('r', 5)
-        .attr('class', 'dot')
-        .attr('data-xvalue', d => yearObject(d))
-        .attr('data-yvalue', d => timeObject(d))
-        .style('fill', d => color(d.Doping.length > 0))
-        .on('mouseover', d => {
-            svg.append('text')
-                .text(d.Name)
-                .attr('id', 'tooltip')
-                .attr('x', xScale(yearObject(d)) + 10)
-                .attr('y', yScale(timeObject(d)) + 5)
-                .attr('data-year', yearObject(d));
-        })
-        .on('mouseout', () => {
-            d3.selectAll('#tooltip').remove();
-        });
+    // svg.selectAll('circle')
+    //     .data(dataset)
+    //     .enter()
+    //     .append('circle')
+    //     .attr('cx', d => xScale(yearObject(d)))
+    //     .attr('cy', d => yScale(timeObject(d)))
+    //     .attr('r', 5)
+    //     .attr('class', 'dot')
+    //     .attr('data-xvalue', d => yearObject(d))
+    //     .attr('data-yvalue', d => timeObject(d))
+    //     .style('fill', d => color(d.Doping.length > 0))
+    //     .on('mouseover', d => {
+    //         svg.append('text')
+    //             .text(d.Name)
+    //             .attr('id', 'tooltip')
+    //             .attr('x', xScale(yearObject(d)) + 10)
+    //             .attr('y', yScale(timeObject(d)) + 5)
+    //             .attr('data-year', yearObject(d));
+    //     })
+    //     .on('mouseout', () => {
+    //         d3.selectAll('#tooltip').remove();
+    //     });
 
-    svg.append('g')
-        .attr('id', 'legend')
-        .selectAll('#legend')
-        .data(color.domain())
-        .enter()
-        .append('text')
-        .attr('x', w - 20)
-        .attr('y', (d, i) => i * 25 + 25)
-        .style('text-anchor', 'end')
-        .style('fill', color)
-        .text(d =>
-            d ? 'Riders with doping allegations' : 'No doping allegations'
-        );
+    // svg.append('g')
+    //     .attr('id', 'legend')
+    //     .selectAll('#legend')
+    //     .data(color.domain())
+    //     .enter()
+    //     .append('text')
+    //     .attr('x', w - 20)
+    //     .attr('y', (d, i) => i * 25 + 25)
+    //     .style('text-anchor', 'end')
+    //     .style('fill', color)
+    //     .text(d =>
+    //         d ? 'Riders with doping allegations' : 'No doping allegations'
+    //     );
 };
